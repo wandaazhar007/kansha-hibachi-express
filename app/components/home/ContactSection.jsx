e65953cf818126c5a5479495e3928230053ca6e3
wanda azhar <wandaazhar@gmail.com>
2023-06-17 16:21:49 -0400

    refactor code

--- app/components/home/ContactSection.jsx
+++ app/components/home/ContactSection.jsx
@@ -32,7 +32,10 @@ const ContactSection = () => {
             </div>
           </div>
           <div className="col-right">
-
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12442.744552741196!2d-93.7368245!3d38.7709022!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c3e304e1e59d4d%3A0xd91636cc2fb9e159!2sKansha%20Hibachi%20Express!5e0!3m2!1sen!2sus!4v1685732677839!5m2!1sen!2sus"
+              style={{ border: 0 }}
+              referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </div>
