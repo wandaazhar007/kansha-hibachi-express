f0a9804daf43327ff2d475e830901703166aaf14
wanda azhar <wandaazhar@gmail.com>
2023-06-28 16:51:26 -0400

    skeleton loading

--- app/components/home/ModalProduct.jsx
+++ app/components/home/ModalProduct.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 
 const ModalProduct = ({ openModal, closeModal, propSlug }) => {
   if (!openModal) return null;
+  const [isLoading, setIsLoading] = useState(true)
   const [nameProduct, setNameProduct] = useState('');
   const [price, setPrice] = useState('');
   const [desc, setDesc] = useState('');
@@ -14,12 +15,21 @@ const ModalProduct = ({ openModal, closeModal, propSlug }) => {
 
 
   const getProductById = async () => {
-    // const response = await axios.get(`http://localhost:2000/products/${propSlug}`);
-    const response = await axios.get(`https://kanshaapi.birojasa-sahabat.com/products/${propSlug}`);
-    setNameProduct(response.data.name);
-    setPrice(response.data.price);
-    setDesc(response.data.desc);
-    setUrlImage(response.data.urlImage);
+    const response = await axios.get(`http://localhost:2000/products/${propSlug}`);
+    // const response = await axios.get(`https://kanshaapi.birojasa-sahabat.com/products/${propSlug}`);
+
+    setTimeout(() => {
+      setNameProduct(response.data.name);
+      setPrice(response.data.price);
+      setDesc(response.data.desc);
+      setUrlImage(response.data.urlImage);
+      setIsLoading(false)
+    }, 500);
+    // setNameProduct(response.data.name);
+    // setPrice(response.data.price);
+    // setDesc(response.data.desc);
+    // setUrlImage(response.data.urlImage);
+    // setIsLoading(false)
     // console.log('get product', response.data)
   }
 
@@ -29,29 +39,58 @@ const ModalProduct = ({ openModal, closeModal, propSlug }) => {
   return (
     <div className="modal-product">
       <div className="box-container">
-        <div className="box">
-          <div className="close-btn" onClick={closeModal}></div>
-          <div className="modal-body">
-            <div className="content-image">
-              <Image src={urlImage} alt="sakura sushi" width={500} height={500} />
+        {isLoading &&
+          <div className="box">
+            <div className="close-btn" onClick={closeModal}></div>
+            <div className="modal-body">
+              <div className="content-image skeleton skeleton-image-modal">
+                {/* <Image src={urlImage} alt="sakura sushi" width={500} height={500} /> */}
+              </div>
+              <h1 className="skeleton skeleton-title-modal"></h1>
+              <h1 className="skeleton skeleton-price-modal"></h1>
+              {/* <p className="desc-detail-product">{desc}</p> */}
             </div>
-            <h1 className="title-detail-product">{nameProduct}</h1>
-            <h1 className="price-detail-product">${price}</h1>
-            {/* <p className="desc-detail-product">{desc}</p> */}
+            <div className="modal-footer">
+              <p className="skeleton skeleton-desc-modal"></p>
+              <p className="skeleton skeleton-desc-modal"></p>
+              <p className="skeleton skeleton-desc-modal"></p>
+              {/* <h1 className="price-detail-product">${price}</h1> */}
+              {/* <button className="close-btn-2" onClick={closeModal}>
+                <FontAwesomeIcon icon={faClose} className="icon" />
+                Close
+              </button> */}
+              {/* <button className="add-to-cart" onClick={() => handleClick(productData.id)}>
+            <FontAwesomeIcon icon={faCartPlus} className="icon" />
+            Add To Cart
+          </button> */}
+            </div>
           </div>
-          <div className="modal-footer">
-            <p className="desc-detail-product">{desc}</p>
-            {/* <h1 className="price-detail-product">${price}</h1> */}
-            <button className="close-btn-2" onClick={closeModal}>
-              <FontAwesomeIcon icon={faClose} className="icon" />
-              Close
-            </button>
-            {/* <button className="add-to-cart" onClick={() => handleClick(productData.id)}>
+        }
+        {!isLoading &&
+          <div className="box">
+            <div className="close-btn" onClick={closeModal}></div>
+            <div className="modal-body">
+              <div className="content-image">
+                <Image src={urlImage} alt="sakura sushi" width={500} height={500} />
+              </div>
+              <h1 className="title-detail-product">{nameProduct}</h1>
+              <h1 className="price-detail-product">${price}</h1>
+              {/* <p className="desc-detail-product">{desc}</p> */}
+            </div>
+            <div className="modal-footer">
+              <p className="desc-detail-product">{desc}</p>
+              {/* <h1 className="price-detail-product">${price}</h1> */}
+              <button className="close-btn-2" onClick={closeModal}>
+                Close
+                <FontAwesomeIcon icon={faClose} className="icon" />
+              </button>
+              {/* <button className="add-to-cart" onClick={() => handleClick(productData.id)}>
               <FontAwesomeIcon icon={faCartPlus} className="icon" />
               Add To Cart
             </button> */}
+            </div>
           </div>
-        </div>
+        }
 
       </div>
     </div>
