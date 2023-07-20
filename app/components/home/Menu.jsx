bebfaa4a0d1f07c253feef60e15ebfe1b52dbf2d
wanda azhar <wandaazhar@gmail.com>
2023-07-20 11:45:46 -0400

    finishing

--- app/components/home/Menu.jsx
+++ app/components/home/Menu.jsx
@@ -1,14 +1,11 @@
 'use client'
 import Image from "next/image";
-import Link from "next/link";
-import { faArrowAltCircleRight, faEye, faHeart, faSquareCaretRight } from "@fortawesome/free-solid-svg-icons";
+import { faSquareCaretRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import ReactPaginate from "react-paginate";
 import ModalProduct from "./ModalProduct";
-import Skeleton from "react-loading-skeleton";
-import 'react-loading-skeleton/dist/skeleton.css';
 
 const Menu = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -101,8 +98,6 @@ const Menu = () => {
   }, [page, keywordButton]);
 
 
-
-  // const testDesc = 'test ipsum dolor sit amet consectetur, adipisicing elit. Dolorum, quas. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, fugit? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam ad saepe libero vel amet nesciunt iste eum fugit nostrum.'
   return (
     <>
       <section className="products">
@@ -122,9 +117,7 @@ const Menu = () => {
               </div>
             </div>
             <div className="search-menu">
-              {/* <input type="text" className="search" placeholder="Search here..." onChange={handleChange} /> */}
               <input type="text" className="search" placeholder="Search here..." onChange={handleSearch} value={keywordSearch} />
-              {/* <p>test</p> */}
               <p>{keywordSearch}</p>
             </div>
 
@@ -154,12 +147,12 @@ const Menu = () => {
               <div className="content">
                 {menus.map((menu, index) => (
                   <>
-                    <div className="box" onClick={() => handleModal(menu.slug, menu.id, menu.name, parseFloat(menu.price))}>
+                    <div className="box" key={menu.id} onClick={() => handleModal(menu.slug, menu.id, menu.name, parseFloat(menu.price))}>
                       <div className="box-images">
-                        <Image height={100} width={100} src={menu.urlImage} alt={menu.name} />
+                        <Image height={300} width={300} src={menu.urlImage} alt={menu.name} />
                       </div>
                       <div className="box-contents">
-                        <h1 className="title" key={menu.id}>{menu.name}</h1>
+                        <h1 className="title" >{menu.name}</h1>
                         <p className="price">${menu.price}</p>
                         <div className="footer-products">
                           <div className="desc">
@@ -191,12 +184,6 @@ const Menu = () => {
                 />
               </div>
             </div>
-
-            {/* <div className="link-menu">
-              <Link href={'/menu'}>
-                <button className="btn-view-product"> <FontAwesomeIcon icon={faArrowAltCircleRight} className="icon" /> View All Menu</button>
-              </Link>
-            </div> */}
 
           </div>
         </div>
