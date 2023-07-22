fff2e11618159b84d788e7cf98d599de1d62b983
wanda azhar <wandaazhar@gmail.com>
2023-07-22 13:46:56 -0400

    refactor code

--- app/components/home/bannerHero/BannerHero.jsx
+++ app/components/home/bannerHero/BannerHero.jsx
@@ -1,8 +1,10 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import './banner-hero.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
 
 const BannerHero = () => {
   return (
@@ -31,15 +33,37 @@ const BannerHero = () => {
         <div className="banner-hero">
 
           <div className="col-left" >
-            <h3>Unleash Your Taste Buds.</h3>
-            <h1>Indulge Kansha Hibachi Delights</h1>
-            <p>Visit us today and experience the thrill of hibachi cuisine brought to life at Kansha Hibachi Express. We look forward to serving you and sharing the joy of our flavorful creations.</p>
-            <div className="cta-hero">
+            <motion.h3
+              initial={{ opacity: 1, x: -200 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+            >Unleash Your Taste Buds.</motion.h3>
+            <motion.h1
+              initial={{ opacity: 1, y: -50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >Indulge Kansha Hibachi Delights</motion.h1>
+            <motion.p
+              initial={{ opacity: 0, y: 0 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 2 }}
+            >Visit us today and experience the thrill of hibachi cuisine brought to life at Kansha Hibachi Express. We look forward to serving you and sharing the joy of our flavorful creations.
+
+            </motion.p>
+            <motion.div
+              initial={{ opacity: 1, x: -200 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1 }}
+              className="cta-hero">
               <Link href="tel:16604299074">
                 <button className="btn-cta-hero"><i className="fas fa-phone"></i> Call Us Now</button>
               </Link>
-            </div>
-            <div className="review-hero">
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 1, y: 100 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+              className="review-hero">
               <div className="stars-review">
                 <i className="fas fa-star"></i>
                 <i className="fas fa-star"></i>
@@ -53,14 +77,18 @@ const BannerHero = () => {
                   5 Stars Ratings on <Link href="https://goo.gl/maps/V9qXQh6mKFZmb15Z7" target="_blank"><span>Google Review</span><FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" /></Link>
                 </p>
               </div>
-            </div>
+            </motion.div>
 
 
           </div>
           <div className="col-right">
-            <div className="image-right">
+            <motion.div
+              initial={{ opacity: 0, x: +200 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 2 }}
+              className="image-right">
               <Image src={'/images/slider_img_1.png'} alt="kansha hibachi express" height={100} width={100} />
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
