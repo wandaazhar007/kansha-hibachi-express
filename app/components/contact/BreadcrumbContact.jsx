a9edc00721c7f7c75d7d2ccfbcc76adde6e7adaa
wanda azhar <wandaazhar@gmail.com>
2023-07-23 02:27:47 -0400

    refactor code

--- app/components/contact/BreadcrumbContact.jsx
+++ app/components/contact/BreadcrumbContact.jsx
@@ -1,3 +1,5 @@
+'use client'
+import { motion } from "framer-motion";
 import Link from "next/link";
 
 const BreadcrumbContact = () => {
@@ -8,11 +10,19 @@ const BreadcrumbContact = () => {
         <div className="tf__breadcrumb_overlay">
           <div className="container">
             <div className="tf__breadcrumb_text">
-              <h1>contact with us</h1>
-              <ul>
+              <motion.h1
+                initial={{ opacity: 1, x: -200 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: 1 }}
+              >Contact Us</motion.h1>
+              <motion.ul
+                initial={{ opacity: 1, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+              >
                 <li><Link href="/">home</Link></li>
                 <li><Link href="/contact">contact</Link></li>
-              </ul>
+              </motion.ul>
             </div>
           </div>
         </div>
