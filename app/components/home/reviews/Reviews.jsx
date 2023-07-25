e4f46f0bffb50095e01f2ccabc86659f471ab0e0
wanda azhar <wandaazhar@gmail.com>
2023-07-25 16:44:26 -0400

    refactor code

--- app/components/home/reviews/Reviews.jsx
+++ app/components/home/reviews/Reviews.jsx
@@ -1,4 +1,5 @@
 'use client'
+import './reviews.scss';
 import { faSquareArrowUpRight, faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
