5bb5a8dad07c0c14c237ea819987fe4af6a13e70
wanda azhar <wandaazhar@gmail.com>
2023-08-10 15:41:18 -0400

    meta description

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -2,8 +2,9 @@ import PrivacyPolicy from "../components/privacy-policy/PrivacyPolicy";
 
 export const metadata = {
   title: 'Privacy Policy - Kansha Hibachi Express',
-  description: 'LLearn about how Kansha Hibachi Express respects your privacy. Our privacy policy outlines how we collect, use, and safeguard your personal information. Your trust matters to us, and we are committed to protecting your data while delivering an exceptional dining experience.',
+  description: 'Learn about how Kansha Hibachi Express respects your privacy. Our privacy policy outlines how we collect, use, and safeguard your personal information. Your trust matters to us, and we are committed to protecting your data while delivering an exceptional dining experience.',
 }
+
 const PagePrivacyPolicy = () => {
   return (
     <PrivacyPolicy />
