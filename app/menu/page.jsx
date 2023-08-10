6d358110553ac7a6a0126338d2633d547e306da6
wanda azhar <wandaazhar@gmail.com>
2023-08-10 04:54:13 -0400

    refactor code

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -1,14 +1,11 @@
-import Image from 'next/image';
 import './menu.scss';
 import BoxMenu from '../components/menu/BoxMenu';
-import Menu from '../components/home/menu/Menu';
 import SearchMenu from '../components/menu/SearchMenu';
 
 const MenuPage = () => {
   return (
     <section className="menuPage">
       <div className="container">
-        {/* <Menu /> */}
         <SearchMenu />
         <h1 className="header">All Our Menu</h1>
         <BoxMenu />
